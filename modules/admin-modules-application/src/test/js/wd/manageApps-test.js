dcf2787a333d22b75e02e20065ee55fd0b5743a5
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2014-11-21 14:55:57 -0500

    Added tests for viewing,installing, and uninstalling apps

---
+++ modules/admin-modules-application/src/test/js/wd/manageApps-test.js
@@ -0,0 +1,39 @@
+/*jslint node: true */
+/* global describe, it, require */
+'use strict';
+
+var shared = require('./shared');
+var asserters = shared.asserters;
+
+describe('Admin UI', function() {
+	shared.setup(this);
+
+  describe('App Management', function() {
+    it("should allow viewing of apps", function() {
+      return this.browser
+        .get("http://localhost:8181/admin/index.html")
+        .waitForElementByCss('#application-grid-layout div.apps-grids div.apps-grid-container.installed div div:nth-child(1) div div.panel-body div.name-version-container div.name span')
+    });
+
+    it("should allow app installation", function() {
+      return this.browser
+        .get("http://localhost:8181/admin/index.html")
+        .waitForElementByCss('#application-grid-buttons div div.application-function-btn div div.btn.edit-toggle').click()
+        .waitForElementByCss('#application-grid-layout div.apps-grids div.show-in-edit-mode', asserters.isDisplayed)
+        .elementByCss('#application-grid-layout div.apps-grids div.show-in-edit-mode div div div:nth-child(1) div div.panel-body div.card-overlay.bottom.show-in-edit-mode.action-button-group div div span:nth-child(1) i.fa.fa-play.fa-stack-1x.fa-inverse').click()
+        .waitForElementByCss('#content-app-startModal', asserters.isDisplayed)
+        .elementByCss('#content-app-startModal div div div.modal-footer div button.btn.btn-primary.startAppConfirm', asserters.isDisplayed).click()
+        .waitForElementByCss('#application-grid-layout div.apps-grids div.apps-grid-container.installed div div:nth-child(3) div')
+    });
+
+    it("should allow app uninstallation", function() {
+      return this.browser
+      .get("http://localhost:8181/admin/index.html")
+      .waitForElementByCss('#application-grid-buttons div div.application-function-btn div div.btn.edit-toggle').click()
+      .waitForElementByCss('#application-grid-layout div.apps-grids div.apps-grid-container.installed div div:nth-child(3) div div.panel-body div.card-overlay.bottom.show-in-edit-mode.action-button-group div div span:nth-child(1) i.fa.fa-pause.fa-stack-1x').click()
+      .waitForElementByCss('#content-app-stopModal', asserters.isDisplayed)
+      .elementByCss('#content-app-stopModal div div div.modal-footer div button.btn.btn-primary.stopAppConfirm').click()
+      .waitForElementByCss('#application-grid-layout div.apps-grids div.show-in-edit-mode div div div:nth-child(1) div div.panel-body div.card-overlay.bottom.show-in-edit-mode.action-button-group div div span:nth-child(1) i.fa.fa-play.fa-stack-1x.fa-inverse')
+    });
+  });
+});
