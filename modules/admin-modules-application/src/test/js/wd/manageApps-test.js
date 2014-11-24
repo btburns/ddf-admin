3639cb308ba63b4ba86995be9387886d23a8e6ff
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2014-11-23 21:11:50 -0500

    Added app removal test

--- modules/admin-modules-application/src/test/js/wd/manageApps-test.js
+++ modules/admin-modules-application/src/test/js/wd/manageApps-test.js
@@ -28,12 +28,23 @@ describe('Admin UI', function() {
 
     it("should allow app uninstallation", function() {
       return this.browser
-      .get("http://localhost:8181/admin/index.html")
-      .waitForElementByCss('#application-grid-buttons div div.application-function-btn div div.btn.edit-toggle').click()
-      .waitForElementByCss('#application-grid-layout div.apps-grids div.apps-grid-container.installed div div:nth-child(3) div div.panel-body div.card-overlay.bottom.show-in-edit-mode.action-button-group div div span:nth-child(1) i.fa.fa-pause.fa-stack-1x').click()
-      .waitForElementByCss('#content-app-stopModal', asserters.isDisplayed)
-      .elementByCss('#content-app-stopModal div div div.modal-footer div button.btn.btn-primary.stopAppConfirm').click()
-      .waitForElementByCss('#application-grid-layout div.apps-grids div.show-in-edit-mode div div div:nth-child(1) div div.panel-body div.card-overlay.bottom.show-in-edit-mode.action-button-group div div span:nth-child(1) i.fa.fa-play.fa-stack-1x.fa-inverse')
+        .get("http://localhost:8181/admin/index.html")
+        .waitForElementByCss('#application-grid-buttons div div.application-function-btn div div.btn.edit-toggle').click()
+        .waitForElementByCss('#application-grid-layout div.apps-grids div.apps-grid-container.installed div div:nth-child(3) div div.panel-body div.card-overlay.bottom.show-in-edit-mode.action-button-group div div span:nth-child(1) i.fa.fa-pause.fa-stack-1x').click()
+        .waitForElementByCss('#content-app-stopModal', asserters.isDisplayed)
+        .elementByCss('#content-app-stopModal div div div.modal-footer div button.btn.btn-primary.stopAppConfirm').click()
+        .waitForElementByCss('#application-grid-layout div.apps-grids div.show-in-edit-mode div div div:nth-child(1) div div.panel-body div.card-overlay.bottom.show-in-edit-mode.action-button-group div div span:nth-child(1) i.fa.fa-play.fa-stack-1x.fa-inverse')
+    });
+
+    it("should allow app removals", function() {
+      return this.browser
+        .get("http://localhost:8181/admin/index.html")
+        .waitForElementByCss('#application-grid-buttons div div.application-function-btn div div.btn.edit-toggle').click()
+        .waitForElementByCss('#application-grid-layout div.apps-grids div.show-in-edit-mode div div div:nth-child(1) div div.panel-body div.card-overlay.bottom.show-in-edit-mode.action-button-group div div span:nth-child(5)').click()
+        .waitForElementByCss('#content-app-removeModal', asserters.isDisplayed)
+        .elementByCss('#content-app-removeModal div div div.modal-footer div button.btn.btn-primary.removeAppConfirm').click()
+        .waitForElementByCss('#application-grid-layout div.apps-grids div.show-in-edit-mode div div div:nth-child(1) div div.panel-body div.card-overlay.bottom.show-in-edit-mode.action-button-group div div span:nth-child(5)', asserters.isNotDisplayed)
+        .elementByCss('#application-outline > div.status-container > div.application-status').text.should.become('Remove complete.')
     });
   });
 });
